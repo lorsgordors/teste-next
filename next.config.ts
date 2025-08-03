9764bb6aca1419b416852363d9f3e87bdf642868
lorsgordors <ld0134720@gmail.com>
2025-08-03 16:13:09 -0300

    refactor: convert next.config.ts to TypeScript and update export syntax

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true, // necessário se você estiver usando <Image>
-},
-  basePath: '/teste-next', // <-- muito importante
-  trailingSlash: true,     // melhora a compatibilidade com GH Pages
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
