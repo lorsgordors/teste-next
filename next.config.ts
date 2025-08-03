340b2d06bfa9152644f29946cef7ed0fe8510f5f
lorsgordors <ld0134720@gmail.com>
2025-08-03 16:07:58 -0300

    refactor: update next.config.ts to use CommonJS syntax and add configuration options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // necessário se você estiver usando <Image>
+},
+  basePath: '/teste-next', // <-- muito importante
+  trailingSlash: true,     // melhora a compatibilidade com GH Pages
 };
 
-export default nextConfig;
+module.exports = nextConfig;
